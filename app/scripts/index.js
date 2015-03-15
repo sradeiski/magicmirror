081a26e4483dba0bb37bd3bd71e9b5977f2d8dd7
Sebastian Radeiski <sradeiski@gmail.com>
2015-03-15 15:24:28 +0100

    Fixed broken test setup. Aded dummy test case.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,7 +3,6 @@
 angular.module('magicmirror', [
   require('./calendar').name,
   require('./clock').name,
-  require('./clock').name,
   require('./motivational').name,
   require('./news').name,
   require('./weather').name
---
+++ test/spec/services/mirrorTests.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var dummyVar = 'test';
+
+describe('Controller: MainCtrl', function () {
+
+  it('dummy', function () {
+    expect(dummyVar).toBeDefined();
+  });
+});

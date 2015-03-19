0a19b36e39f5cd9a24d3e2b863da131441572f9c
Sebastian Radeiski <sradeiski@gmail.com>
2015-03-19 20:16:52 +0100

    Finished weather module.

--- nodeserver/routes/weather.js
+++ nodeserver/routes/weather.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET weather for today */
 router.get('/today', function(req, res, next) {
@@ -7,7 +8,8 @@ router.get('/today', function(req, res, next) {
       mode,
       lang,
       units,
-      weatherApiUrl;
+      weatherApiUrl,
+      weatherRequestUrl;
 
   cityAndCountry = req.query.cityAndCountry;
   mode = req.query.mode;
@@ -15,49 +17,57 @@ router.get('/today', function(req, res, next) {
   units = req.query.units;
   weatherApiUrl = 'http://api.openweathermap.org/data/2.5/weather';
 
-  
+  weatherRequestUrl =  weatherApiUrl +
+                      '?' + 'q' + '=' + cityAndCountry + 
+                      '&' + 'mode' + '=' + mode + 
+                      '&' + 'lang' + '=' + lang + 
+                      '&' + 'units' + '=' + units;
 
-  var dummyResponse = 
-    weatherApiUrl +
-    '?' + 'q' + '=' + cityAndCountry + 
-    '&' + 'mode' + '=' + mode + 
-    '&' + 'lang' + '=' + lang + 
-    '&' + 'units' + '=' + units;
-
-
-  
-
-  res.status(200).send(dummyResponse);
+  //make request to get weather data
+  request(weatherRequestUrl, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.set('Content-Type', response.headers['content-type']).send(body);
+    } else {
+      console.log(error);
+      res.statusCode(response.statusCode).send(error);
+    }
+  });
 });
 
 /* GET weather forecast */
 router.get('/forecast', function(req, res, next) {
   var cityAndCountry,
       mode,
+      numberOfDays,
       lang,
       units,
-      weatherApiUrl;
+      weatherApiUrl,
+      weatherRequestUrl;
 
   cityAndCountry = req.query.cityAndCountry;
   mode = req.query.mode;
+  numberOfDays = req.query.numberOfDays;
   lang = req.query.lang;
   units = req.query.units;
   weatherApiUrl = 'http://api.openweathermap.org/data/2.5/forecast/daily';
 
-  
+  weatherRequestUrl =  weatherApiUrl +
+                      '?' + 'q' + '=' + cityAndCountry + 
+                      '&' + 'mode' + '=' + mode + 
+                      '&' + 'numberOfDays' + '=' + numberOfDays + 
+                      '&' + 'lang' + '=' + lang + 
+                      '&' + 'units' + '=' + units;
 
-
-  var dummyResponse = 
-    weatherApiUrl +
-    '?' + 'q' + '=' + cityAndCountry + 
-    '&' + 'mode' + '=' + mode + 
-    '&' + 'lang' + '=' + lang + 
-    '&' + 'units' + '=' + units;
-
-
-  
-
-  res.status(200).send(dummyResponse);
+  //make request to get weather data
+  request(weatherRequestUrl, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.set('Content-Type', response.headers['content-type']).send(body);
+    } else {
+      console.log(error);
+      res.statusCode(response.statusCode).send(error);
+    }
+  });
 });
 
 module.exports = router;
+
